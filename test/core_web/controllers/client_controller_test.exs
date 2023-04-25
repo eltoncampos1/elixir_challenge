e916dc50cb24a2fbc69610f09360b0f28cc4be65
Elton Campos <eltoncampos36@gmail.com>
2023-04-25 19:16:35 -0300

    test: signup

--- test/core_web/controllers/client_controller_test.exs
+++ test/core_web/controllers/client_controller_test.exs
@@ -16,10 +16,63 @@ defmodule CoreWeb.ClientControllerTest do
         }
       }
 
-      conn
-      |> post(Routes.client_path(conn, :signup), params)
-      |> json_response(200)
-      |> IO.inspect()
+      response =
+        conn
+        |> post(Routes.client_path(conn, :signup), params)
+        |> json_response(200)
+
+      assert %{"email" => "valid@email.com", "id" => _user_id} = response
+    end
+
+    test "return 400 with errors with wrong params on client", %{conn: conn} do
+      params = %{
+        client: %{
+          email: "invalidemail.com",
+          password: "1234"
+        },
+        address: %{
+          cep: "00000000",
+          state: "SP",
+          city: "SP",
+          number: "00"
+        }
+      }
+
+      response =
+        conn
+        |> post(Routes.client_path(conn, :signup), params)
+        |> json_response(422)
+
+      assert %{
+               "errors" => %{
+                 "email" => ["has invalid format"],
+                 "password" => ["should be at least 6 character(s)"]
+               },
+               "status" => "failure"
+             } = response
+    end
+
+    test "return 400 with errors with wrong params o address", %{conn: conn} do
+      params = %{
+        client: %{
+          email: "valid@email.com",
+          password: "12345678"
+        },
+        address: %{
+          cep: "00000",
+          state: "SP",
+          city: "SP",
+          number: "00"
+        }
+      }
+
+      response =
+        conn
+        |> post(Routes.client_path(conn, :signup), params)
+        |> json_response(422)
+
+      assert %{"errors" => %{"cep" => ["should be 8 character(s)"]}, "status" => "failure"} =
+               response
     end
   end
 end
