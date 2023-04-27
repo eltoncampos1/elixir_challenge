d31f2942a33a510b04558f99306625b31fd0171a
Elton Campos <eltoncampos36@gmail.com>
2023-04-27 10:04:00 -0300

    feat: create plug to check id format

---
+++ lib/core_web/Plugs/uuid_checker.ex
@@ -0,0 +1,12 @@
+defmodule CoreWeb.Plugs.UUIDChecker do
+  import Plug.Conn
+
+  def init(default), do: default
+
+  def call(%Plug.Conn{params: %{"id" => id}} = conn, _default) do
+    case Ecto.UUID.cast(id) do
+      {:ok, _uuid} -> conn
+      _ -> conn |> resp(401, Jason.encode!(%{"error" => "unauthorized"})) |> halt()
+    end
+  end
+end
